b2046606dee6307c82c04f0ebe1e3612db797cae
iliasMahraoui <2143218@talnet.nl>
2021-11-22 11:33:07 +0100

    df

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,5 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+ef foodsel(foodsel):
+    print("Beste Eten",foodsel)
+foodsel("Pizza")
+foodsel("Kip")
+foodsel("CousCous")
